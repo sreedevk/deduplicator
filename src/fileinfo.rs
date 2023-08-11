351362865518a7d65df280f089e4427afd34e11d
sreedev <sreedevpadmakumar@gmail.com>
2023-08-10 20:16:37 -0400

    code documentation & reorg

--- src/fileinfo.rs
+++ src/fileinfo.rs
@@ -4,6 +4,12 @@ use std::fs;
 use std::hash::Hasher;
 use std::{fs::Metadata, path::PathBuf};
 
+/// FileInfo is a struct that is used to represent a file on disk.
+/// it contains the following information:
+///     path - the absolute path to the file 
+///     filemeta - file metadata of the file (std::fs::MetaData)
+///     hash - Option of hash generated using the fxhash library
+///     size - size of file obtained from the MetaData of the file
 #[derive(Debug, Clone)]
 pub struct FileInfo {
     pub path: PathBuf,
@@ -13,6 +19,8 @@ pub struct FileInfo {
 }
 
 impl FileInfo {
+    /// returns a copy of self with the `hash` field replaced with Some(String) containing the hash
+    /// of the contents of the file
     pub fn hash(&self) -> Result<Self> {
         let file = fs::File::open(self.path.clone())?;
         let mapper = unsafe { Mmap::map(&file)? };
@@ -28,6 +36,10 @@ impl FileInfo {
         })
     }
 
+    /// accepts the path of the file & returns a new FileInfo struct instance that contains
+    /// metadata, size & path of the file.
+    /// The hash is set to None at this stage as it will only be calculated when its absolutely
+    /// required.
     pub fn new(path: PathBuf) -> Result<Self> {
         let filemeta = std::fs::metadata(path.clone())?;
         Ok(Self {
--- src/processor.rs
+++ src/processor.rs
@@ -6,6 +6,8 @@ use std::{time::Duration, borrow::Cow};
 
 use crate::fileinfo::FileInfo;
 
+/// different phases through which deduplicator progresses during the process of detecting
+/// duplicates.
 #[derive(Debug, Clone)]
 pub enum State {
     Initial,
@@ -13,6 +15,7 @@ pub enum State {
     HashWise,
 }
 
+/// Processor struct contains the set of files that are being worked on along with the current state of the process.
 #[derive(Debug, Clone)]
 pub struct Processor {
     pub files: Vec<FileInfo>,
@@ -25,46 +28,6 @@ impl Processor {
             files,
             state: State::Initial,
         }
-    }
-
-    pub fn hashwise(&self) -> Result<Self> {
-        if self.files.is_empty() {
-            return Ok(self.clone());
-        }
-
-        let progress_style = ProgressStyle::with_template("[{elapsed_precise}] {bar:40.cyan/blue} {pos:>7}/{len:7} {msg}")?;
-        let progress_bar = ProgressBar::new(self.files.len() as u64);
-        progress_bar.set_style(progress_style);
-        progress_bar.enable_steady_tick(Duration::from_millis(50));
-        progress_bar.set_message("indexing file hashes");
-
-        let duplicates_table: DashMap<String, Vec<FileInfo>> = DashMap::new();
-        self.files
-            .clone()
-            .into_par_iter()
-            .progress_with(progress_bar)
-            .with_finish(ProgressFinish::WithMessage(Cow::from("indexed files hashes")))
-            .map(|file| file.hash())
-            .filter_map(Result::ok)
-            .for_each(|file| {
-                duplicates_table
-                    .entry(file.hash.clone().unwrap_or_default())
-                    .and_modify(|fileset| fileset.push(file.clone()))
-                    .or_insert_with(|| vec![file]);
-            });
-
-        let files = duplicates_table
-            .into_read_only()
-            .values()
-            .cloned()
-            .filter(|subfiles| subfiles.len() > 1)
-            .flatten()
-            .collect::<Vec<FileInfo>>();
-
-        Ok(Self {
-            files,
-            state: State::HashWise,
-        })
     }
 
     pub fn sizewise(&self) -> Result<Self> {
@@ -102,6 +65,47 @@ impl Processor {
         Ok(Self {
             files,
             state: State::SizeWise,
+        })
+    }
+
+    /// hashwise is the last 
+    pub fn hashwise(&self) -> Result<Self> {
+        if self.files.is_empty() {
+            return Ok(self.clone());
+        }
+
+        let progress_style = ProgressStyle::with_template("[{elapsed_precise}] {bar:40.cyan/blue} {pos:>7}/{len:7} {msg}")?;
+        let progress_bar = ProgressBar::new(self.files.len() as u64);
+        progress_bar.set_style(progress_style);
+        progress_bar.enable_steady_tick(Duration::from_millis(50));
+        progress_bar.set_message("indexing file hashes");
+
+        let duplicates_table: DashMap<String, Vec<FileInfo>> = DashMap::new();
+        self.files
+            .clone()
+            .into_par_iter()
+            .progress_with(progress_bar)
+            .with_finish(ProgressFinish::WithMessage(Cow::from("indexed files hashes")))
+            .map(|file| file.hash())
+            .filter_map(Result::ok)
+            .for_each(|file| {
+                duplicates_table
+                    .entry(file.hash.clone().unwrap_or_default())
+                    .and_modify(|fileset| fileset.push(file.clone()))
+                    .or_insert_with(|| vec![file]);
+            });
+
+        let files = duplicates_table
+            .into_read_only()
+            .values()
+            .cloned()
+            .filter(|subfiles| subfiles.len() > 1)
+            .flatten()
+            .collect::<Vec<FileInfo>>();
+
+        Ok(Self {
+            files,
+            state: State::HashWise,
         })
     }
 }
